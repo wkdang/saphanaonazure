6652156c59c6aa7c82111d7877137c2cf4d45d96
jgardner04 <jonathan.gardner04@gmail.com>
2017-08-02 10:14:37 -0500

    Added custom script extension

---
+++ preReqInstall.sh
@@ -0,0 +1,4 @@
+wget https://github.com/Microsoft/omi/releases/download/v1.1.0-0/omi-1.1.0.ssl_100.x64.rpm
+wget https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_100.x64.rpm
+
+sudo rpm -Uvh omi-1.1.0.ssl_100.x64.rpm dsc-1.1.1-294.ssl_100.x64.rpm
