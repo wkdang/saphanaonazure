d8d106a688164a3a847f165aa7ac51a5bed72555
Jonathan Gardner (MSFT) <jonathan.gardner04@gmail.com>
2017-08-16 18:07:55 -0500

    Corected dotnet install

--- preReqInstall.sh
+++ preReqInstall.sh
@@ -1,5 +1,4 @@
 # Install PowerShell
-
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.5/powershell-6.0.0_beta.5-1.suse.42.1.x86_64.rpm
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo zypper info libuuid-devel
@@ -7,9 +6,9 @@ sudo rpm -Uvh --nodeps ./powershell-6.0.0_beta.5-1.suse.42.1.x86_64.rpm
 
 # Install .NET Core and AzCopy
 sudo zypper install libunwind libicu
-curl -ssl -o dotnet.tar.gz https://aka.ms/dotnet-sdk-2.0.0-linux-x64
+curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?linkid=848824
 sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
-sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+sudo ln -s /opt/dotnet/dotnet /usr/bin
 
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
 tar -xf azcopy.tar.gz
