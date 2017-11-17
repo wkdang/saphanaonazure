e96be737f1c28623f7050cc8948eb0a2d5ce25d1
Claudia del Hierro <claudel@microsoft.com>
2017-11-17 15:47:34 -0800

    Add files via upload

--- preReqInstall.sh
+++ preReqInstall.sh
@@ -1,12 +1,9 @@
-AZ_URI=$2
-AZ_KEY=$1
-DSC_CONFIG_NAME=$3
+Uri=$1
+HANAUSR=$2
+HANAPWD=$3
+HANASID=$4
+HANANUMBER=$5
 
-# Install PowerShell
-wget https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.5/powershell-6.0.0_beta.5-1.suse.42.1.x86_64.rpm
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo zypper info libuuid-devel
-sudo rpm -Uvh --nodeps ./powershell-6.0.0_beta.5-1.suse.42.1.x86_64.rpm
 
 #install hana prereqs
 sudo zypper install -y glibc-2.22-51.6
@@ -34,21 +31,117 @@ wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
 tar -xf azcopy.tar.gz
 sudo ./install.sh
 
-# Install DSC for Linux
-wget https://github.com/Microsoft/omi/releases/download/v1.1.0-0/omi-1.1.0.ssl_100.x64.rpm
-wget https://github.com/Microsoft/PowerShell-DSC-for-Linux/releases/download/v1.1.1-294/dsc-1.1.1-294.ssl_100.x64.rpm
+sudo zypper se -t pattern
+sudo zypper in -t pattern sap-hana
 
-sudo rpm -Uvh omi-1.1.0.ssl_100.x64.rpm dsc-1.1.1-294.ssl_100.x64.rpm
+# step2
+echo $Uri >> /tmp/url.txt
 
-#do more SAP configuration
-tuned-adm profile sap-hana
-systemctl start tuned
-systemctl enable tuned
-saptune solution apply HANA
-saptune daemon start
-echo 'GRUB_CMDLINE_LINUX_DEFAULT="transparent_hugepage=never numa_balancing=disable intel_idle.max_cstate=1 processor.max_cstate=1"' >>/etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
-echo 1 > /root/boot-requested
+cp -f /etc/waagent.conf /etc/waagent.conf.orig
+sedcmd="s/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g"
+sedcmd2="s/ResourceDisk.SwapSizeMB = 16384/ResourceDisk.SwapSizeMB = 16384/g"
+cat /etc/waagent.conf | sed $sedcmd | sed $sedcmd2 > /etc/waagent.conf.new
+cp -f /etc/waagent.conf.new /etc/waagent.conf
 
-# Register Node for Azure Automation DSC Management
-sudo /opt/microsoft/dsc/Scripts/Register.py --RegistrationKey $AZ_KEY --ServerURL $AZ_URI --ConfigurationName $DSC_CONFIG_NAME --RefreshFrequencyMins 5 --ConfigurationMode ApplyAndAutoCorrect --ConfigurationModeFrequencyMins 10 --RefreshMode Pull
+cp -f /etc/systemd/login.conf.d/sap.conf /etc/systemd/login.conf.d/sap.conf.orig
+sedcmd="s/[login]`n
+UserTasksMax=infinity`n/[login]`n
+UserTasksMax=infinity`n/g"
+cat /etc/systemd/login.conf.d/sap.conf | sed $sedcmd > //etc/systemd/login.conf.d/sap.conf.new
+cp -f /etc/systemd/login.conf.d/sap.conf.new /etc/systemd/login.conf.d/sap.conf
+
+echo "logicalvols start" >> /tmp/parameter.txt
+pvcreate /dev/sd[cdefg]
+vgcreate hanavg /dev/sd[fg]
+lvcreate -l 80%FREE -n datalv hanavg
+lvcreate -l 20%FREE -n loglv hanavg
+mkfs.xfs /dev/hanavg/datalv
+mkfs.xfs /dev/hanavg/loglv
+echo "logicalvols end" >> /tmp/parameter.txt
+
+
+#!/bin/bash
+echo "logicalvols2 start" >> /tmp/parameter.txt
+vgcreate sharedvg /dev/sdc 
+vgcreate usrsapvg /dev/sdd
+vgcreate backupvg /dev/sde  
+ 
+lvcreate -l 100%FREE -n sharedlv sharedvg 
+lvcreate -l 100%FREE -n backuplv backupvg 
+lvcreate -l 100%FREE -n usrsaplv usrsapvg 
+mkfs -t xfs /dev/sharedvg/sharedlv 
+mkfs -t xfs /dev/backupvg/backuplv 
+mkfs -t xfs /dev/usrsapvg/usrsaplv
+echo "logicalvols2 end" >> /tmp/parameter.txt
+
+
+#!/bin/bash
+echo "mounthanashared start" >> /tmp/parameter.txt
+mount -t xfs /dev/sharedvg/sharedlv /hana/shared
+mount -t xfs /dev/backupvg/backuplv /hana/backup 
+mount -t xfs /dev/usrsapvg/usrsaplv /usr/sap
+mount -t xfs /dev/hanavg/datalv /hana/data
+mount -t xfs /dev/hanavg/loglv /hana/log 
+mkdir /hana/data/sapbits
+echo "mounthanashared end" >> /tmp/parameter.txt
+
+if [ ! -d "/hana/data/sapbits" ]
+ then
+ mkdir "/hana/data/sapbits"
+fi
+
+
+
+#!/bin/bash
+cd /hana/data/sapbits
+echo "hana download start" >> /tmp/parameter.txt
+/usr/bin/wget --quiet $Uri/SapBits/md5sums
+/usr/bin/wget --quiet $Uri/SapBits/51052325_part1.exe
+/usr/bin/wget --quiet $Uri/SapBits/51052325_part2.rar
+/usr/bin/wget --quiet $Uri/SapBits/51052325_part3.rar
+/usr/bin/wget --quiet $Uri/SapBits/51052325_part4.rar
+/usr/bin/wget --quiet $Uri/SapBits/hdbinst.cfg
+echo "hana download end" >> /tmp/parameter.txt
+
+date >> /tmp/testdate
+cd /hana/data/sapbits
+
+echo "hana unrar start" >> /tmp/parameter.txt
+#!/bin/bash
+cd /hana/data/sapbits
+unrar -inul x 51052325_part1.exe
+echo "hana unrar end" >> /tmp/parameter.txt
+
+echo "hana prepare start" >> /tmp/parameter.txt
+cd /hana/data/sapbits
+sbfilecount=`ls -1 | grep 51052325 | grep -v part| wc -l`
+if [ $sbfilecount -gt 0 ]
+then
+    ssfilecount=`find /hana/data/sapbits/51052325 | wc -l`
+    if [ $ssfilecount -gt 5365 ]
+    then
+        exit 0
+    else
+        exit 1
+    fi
+else
+    exit 1
+fi
+
+#!/bin/bash
+cd /hana/data/sapbits
+myhost=`hostname`
+sedcmd="s/REPLACE-WITH-HOSTNAME/$myhost/g"
+sedcmd2="s/\/hana\/shared\/sapbits\/51052325/\/hana\/data\/sapbits\/51052325/g"
+sedcmd3="s/root_user=root/root_user=$HANAUSR/g"
+sedcmd4="s/root_password=AweS0me@PW/root_password=$HANAPWD/g"
+sedcmd5="s/sid=H10/sid=$HANASID/g"
+sedcmd6="s/number=00/number=$HANANUMBER/g"
+cat hdbinst.cfg | sed $sedcmd | sed $sedcmd2 | sed $sedcmd3 | sed $sedcmd4 | sed $sedcmd5 | sed $sedcmd6 > hdbinst-local.cfg
+echo "hana preapre end" >> /tmp/parameter.txt
+
+#!/bin/bash
+echo "install hana start" >> /tmp/parameter.txt
+cd /hana/data/sapbits/51052325/DATA_UNITS/HDB_LCM_LINUX_X86_64
+/hana/data/sapbits/51052325/DATA_UNITS/HDB_LCM_LINUX_X86_64/hdblcm -b --configfile /hana/data/sapbits/hdbinst-local.cfg
+echo "install hana end" >> /tmp/parameter.txt
