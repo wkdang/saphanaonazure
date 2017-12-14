84c8eb7436a5188f3f389deeb69a58d4cfc8b642
Claudia del Hierro <claudel@microsoft.com>
2017-12-14 08:25:51 -0800

    Delete hanastudio.bat

--- SapBits/hanastudio.bat
+++
@@ -1,4 +0,0 @@
-set PATH=%PATH%C:\Program Files\jdk-9\bin;
-set HDB_INSTALLER_TRACE_FILE=C:\Users\testuser\Documents\hdbinst.log
-
-hdbinst -a C:\SAPbits\SAP_HANA_STUDIO\studio -b --path="C:\Program Files\sap\hdbstudio"
