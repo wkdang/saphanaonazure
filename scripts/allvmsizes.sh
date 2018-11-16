b3339d394335d780752e7129d64f982751681cb1
Ross Sponholtz <rsponholtz@hotmail.com>
2018-11-16 14:53:04 -0800

    Update allvmsizes.sh

--- scripts/allvmsizes.sh
+++ scripts/allvmsizes.sh
@@ -206,7 +206,7 @@ echo "/dev/mapper/logvg-loglv /hana/log xfs defaults 0 0" >> /etc/fstab
 echo "logicalvols end" >> /tmp/parameter.txt
 fi
 
-if [ $VMSIZE == "Standard_M128ms" ]; then
+if [ $VMSIZE == "Standard_M128ms" || [ $VMSIZE == "Standard_M208ms_v2" ]; then
 
   # this assumes that 6 disks are attached at lun 0 through 5
   echo "Creating partitions and physical volumes"
