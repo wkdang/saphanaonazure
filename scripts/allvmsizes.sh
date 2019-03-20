e06f37f6738723a7a22c86e7adc8f84dba63937e
wkdang <36007806+wkdang@users.noreply.github.com>
2019-03-20 06:50:45 +0100

    Add files via upload

--- scripts/allvmsizes.sh
+++ scripts/allvmsizes.sh
@@ -1,210 +1,116 @@
-set -x
+#!/bin/bash
+#set -e
 
-Uri=${1}
-HANAVER=${2}
-HANAUSR=${3}
-HANAPWD=${4}
-HANASID=${5}
-HANANUMBER=${6}
-vmSize=${7}
-SUBEMAIL=${8}
-SUBID=${9}
-SUBURL=${10}
+Uri=$1
+HANAUSR=$2
+HANAPWD=$3
+HANASID=$4
+HANANUMBER=$5
+HANAVERS=$6
+OS=$7
+vmSize=$8
 
-#if needed, register the machine
-if [ "$SUBEMAIL" != "" ]; then
-  if [ "$SUBURL" != "" ]; then 
-   SUSEConnect -e $SUBEMAIL -r $SUBID --url $SUBURL
-  else 
-   SUSEConnect -e $SUBEMAIL -r $SUBID
-  fi
-fi
+echo $1 >> /tmp/parameter.txt
+echo $2 >> /tmp/parameter.txt
+echo $3 >> /tmp/parameter.txt
+echo $4 >> /tmp/parameter.txt
+echo $5 >> /tmp/parameter.txt
+echo $6 >> /tmp/parameter.txt
+echo $7 >> /tmp/parameter.txt
+
 
 #get the VM size via the instance api
 VMSIZE=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/vmSize?api-version=2017-08-01&format=text"`
 
 
+if [ "$7" == "RHEL" ]; then
+	echo "Start REHL prerequisite" >> /tmp/parameter.txt
+	yum -y groupinstall base
+	yum -y install gtk2 libicu xulrunner sudo tcsh libssh2 expect cairo graphviz iptraf-ng 
+	yum -y install compat-sap-c++-6
+	sudo mkdir -p /hana/{data,log,shared,backup}
+	sudo mkdir /usr/sap
+	sudo mkdir -p /hana/data/{sapbitslocal,sapbits}
+	yum -y install tuned-profiles-sap-hana
+	systemctl start tuned
+	systemctl enable tuned
+	tuned-adm profile sap-hana
+	setenforce 0
+	#sed -i 's/\(SELINUX=enforcing\|SELINUX=permissive\)/SELINUX=disabled/g' \ > /etc/selinux/config
+	echo "start SELINUX" >> /tmp/parameter.txt
+	sed -i -e "s/\(SELINUX=enforcing\|SELINUX=permissive\)/SELINUX=disabled/g" /etc/selinux/config
+	echo "end SELINUX" >> /tmp/parameter.txt
+	echo "kernel.numa_balancing = 0" > /etc/sysctl.d/sap_hana.conf
+	ln -s /usr/lib64/libssl.so.1.0.1e /usr/lib64/libssl.so.1.0.1
+	ln -s /usr/lib64/libcrypto.so.0.9.8e /usr/lib64/libcrypto.so.0.9.8
+	ln -s /usr/lib64/libcrypto.so.1.0.1e /usr/lib64/libcrypto.so.1.0.1
+	echo always > /sys/kernel/mm/transparent_hugepage/enabled
+	echo never > /sys/kernel/mm/transparent_hugepage/enabled
+	echo "start Grub" >> /tmp/parameter.txt
+	sedcmd="s/rootdelay=300/rootdelay=300 transparent_hugepage=never intel_idle.max_cstate=1 processor.max_cstate=1/g"
+	sudo sed -i -e "$sedcmd" /etc/default/grub
+	echo "start Grub2" >> /tmp/parameter.txt
+	sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+	echo "End Grub" >> /tmp/parameter.txt
+    echo "@sapsys         soft    nproc   unlimited" >> /etc/security/limits.d/99-sapsys.conf
+	systemctl disable abrtd
+	systemctl disable abrt-ccpp
+	systemctl stop abrtd
+	systemctl stop abrt-ccpp
+	systemctl stop kdump.service
+	systemctl disable kdump.service
+	systemctl stop firewalld
+	systemctl disable firewalld
+	sudo mkdir -p /sources
+	yum -y install cifs-utils
+	# Install Unrar  
+	echo "start RAR" >> /tmp/parameter.txt
+	wget http://www.rarlab.com/rar/unrar-5.0-RHEL5x64.tar.gz 
+	tar -zxvf unrar-5.0-RHEL5x64.tar.gz 
+	cp unrar /usr/bin/ 
+	chmod 755 /usr/bin/unrar 
+	echo "End RAR" >> /tmp/parameter.txt
+	echo "End REHL prerequisite" >> /tmp/parameter.txt
+	
+else
 #install hana prereqs
-zypper install -y glibc-2.22-51.6
-zypper install -y systemd-228-142.1
-zypper install -y unrar
-zypper install -y sapconf
-zypper install -y saptune
-mkdir /etc/systemd/login.conf.d
-mkdir /hana
-mkdir /hana/data
-mkdir /hana/log
-mkdir /hana/shared
-mkdir /hana/backup
-mkdir /usr/sap
+	sudo zypper install -y glibc-2.22-51.6
+	sudo zypper install -y systemd-228-142.1
+	sudo zypper install -y unrar
+	sudo zypper install -y sapconf
+	sudo zypper install -y saptune
+	sudo mkdir /etc/systemd/login.conf.d
+	sudo mkdir -p /hana/{data,log,shared,backup}
+	sudo mkdir /usr/sap
+	sudo mkdir -p /hana/data/{sapbitslocal,sapbits}
 
-zypper in -t pattern -y sap-hana
-saptune solution apply HANA
-saptune daemon start
+
+
+# Install .NET Core and AzCopy
+	sudo zypper install -y libunwind
+	sudo zypper install -y libicu
+	curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?linkid=848824
+	sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+	sudo ln -s /opt/dotnet/dotnet /usr/bin
+
+	wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
+	tar -xf azcopy.tar.gz
+	sudo ./install.sh
+
+	sudo zypper se -t pattern
+	sudo zypper --non-interactive in -t pattern sap-hana 
+fi
+
 
 # step2
 echo $Uri >> /tmp/url.txt
 
 cp -f /etc/waagent.conf /etc/waagent.conf.orig
 sedcmd="s/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g"
-sedcmd2="s/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=2048/g"
+sedcmd2="s/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=163840/g"
 cat /etc/waagent.conf | sed $sedcmd | sed $sedcmd2 > /etc/waagent.conf.new
 cp -f /etc/waagent.conf.new /etc/waagent.conf
-
-#don't restart waagent, as this will kill the custom script.
-#service waagent restart
-
-# this assumes that 5 disks are attached at lun 0 through 4
-echo "Creating partitions and physical volumes"
-pvcreate -ff -y /dev/disk/azure/scsi1/lun0   
-pvcreate -ff -y  /dev/disk/azure/scsi1/lun1
-pvcreate -ff -y  /dev/disk/azure/scsi1/lun2
-pvcreate -ff -y  /dev/disk/azure/scsi1/lun3
-pvcreate -ff -y  /dev/disk/azure/scsi1/lun4
-pvcreate -ff -y  /dev/disk/azure/scsi1/lun5
-
-if [ $VMSIZE == "Standard_E16s_v3" ] || [ "$VMSIZE" == "Standard_E32s_v3" ] || [ "$VMSIZE" == "Standard_E64s_v3" ] || [ "$VMSIZE" == "Standard_GS5" ] || [ "$VMSIZE" == "Standard_M32ts" ] || [ "$VMSIZE" == "Standard_M32ls" ] || [ "$VMSIZE" == "Standard_M64ls" ] || [ $VMSIZE == "Standard_DS14_v2" ] ; then
-echo "logicalvols start" >> /tmp/parameter.txt
-  #shared volume creation
-  sharedvglun="/dev/disk/azure/scsi1/lun0"
-  vgcreate sharedvg $sharedvglun
-  lvcreate -l 100%FREE -n sharedlv sharedvg 
- 
-  #usr volume creation
-  usrsapvglun="/dev/disk/azure/scsi1/lun1"
-  vgcreate usrsapvg $usrsapvglun
-  lvcreate -l 100%FREE -n usrsaplv usrsapvg
-
-  #backup volume creation
-  backupvglun="/dev/disk/azure/scsi1/lun2"
-  vgcreate backupvg $backupvglun
-  lvcreate -l 100%FREE -n backuplv backupvg 
-
-  #data volume creation
-  datavg1lun="/dev/disk/azure/scsi1/lun3"
-  datavg2lun="/dev/disk/azure/scsi1/lun4"
-  datavg3lun="/dev/disk/azure/scsi1/lun5"
-  vgcreate datavg $datavg1lun $datavg2lun $datavg3lun
-  PHYSVOLUMES=3
-  STRIPESIZE=64
-  lvcreate -i$PHYSVOLUMES -I$STRIPESIZE -l 70%FREE -n datalv datavg
-  lvcreate -i$PHYSVOLUMES -I$STRIPESIZE -l 100%FREE -n loglv datavg
-  mount -t xfs /dev/datavg/loglv /hana/log 
-  echo "/dev/mapper/datavg-loglv /hana/log xfs defaults 0 0" >> /etc/fstab
-
-  mkfs.xfs /dev/datavg/datalv
-  mkfs.xfs /dev/datavg/loglv
-  mkfs -t xfs /dev/sharedvg/sharedlv 
-  mkfs -t xfs /dev/backupvg/backuplv 
-  mkfs -t xfs /dev/usrsapvg/usrsaplv
-echo "logicalvols end" >> /tmp/parameter.txt
-fi
-
-if [ $VMSIZE == "Standard_M64s" ]; then
-  #this is the medium size
-  # this assumes that 6 disks are attached at lun 0 through 5
-  echo "Creating partitions and physical volumes"
-  pvcreate -ff -y  /dev/disk/azure/scsi1/lun6
-  pvcreate -ff -y  /dev/disk/azure/scsi1/lun7
-  pvcreate -ff -y /dev/disk/azure/scsi1/lun8
-  pvcreate -ff -y /dev/disk/azure/scsi1/lun9
-
-  echo "logicalvols start" >> /tmp/parameter.txt
-  #shared volume creation
-  sharedvglun="/dev/disk/azure/scsi1/lun0"
-  vgcreate sharedvg $sharedvglun
-  lvcreate -l 100%FREE -n sharedlv sharedvg 
- 
-  #usr volume creation
-  usrsapvglun="/dev/disk/azure/scsi1/lun1"
-  vgcreate usrsapvg $usrsapvglun
-  lvcreate -l 100%FREE -n usrsaplv usrsapvg
-
-  #backup volume creation
-  backupvg1lun="/dev/disk/azure/scsi1/lun2"
-  backupvg2lun="/dev/disk/azure/scsi1/lun3"
-  vgcreate backupvg $backupvg1lun $backupvg2lun
-  lvcreate -l 100%FREE -n backuplv backupvg 
-
-  #data volume creation
-  datavg1lun="/dev/disk/azure/scsi1/lun4"
-  datavg2lun="/dev/disk/azure/scsi1/lun5"
-  datavg3lun="/dev/disk/azure/scsi1/lun6"
-  datavg4lun="/dev/disk/azure/scsi1/lun7"
-  vgcreate datavg $datavg1lun $datavg2lun $datavg3lun $datavg4lun
-  PHYSVOLUMES=4
-  STRIPESIZE=64
-  lvcreate -i$PHYSVOLUMES -I$STRIPESIZE -l 100%FREE -n datalv datavg
-
-  #log volume creation
-  logvg1lun="/dev/disk/azure/scsi1/lun8"
-  logvg2lun="/dev/disk/azure/scsi1/lun9"
-  vgcreate logvg $logvg1lun $logvg2lun
-  PHYSVOLUMES=2
-  STRIPESIZE=32
-  lvcreate -i$PHYSVOLUMES -I$STRIPESIZE -l 100%FREE -n loglv logvg
-  mount -t xfs /dev/logvg/loglv /hana/log 
-echo "/dev/mapper/logvg-loglv /hana/log xfs defaults 0 0" >> /etc/fstab
-
-  mkfs.xfs /dev/datavg/datalv
-  mkfs.xfs /dev/logvg/loglv
-  mkfs -t xfs /dev/sharedvg/sharedlv 
-  mkfs -t xfs /dev/backupvg/backuplv 
-  mkfs -t xfs /dev/usrsapvg/usrsaplv
-echo "logicalvols end" >> /tmp/parameter.txt
-fi
-
-if [ $VMSIZE == "Standard_M64ms" ] || [ $VMSIZE == "Standard_M128s" ]; then
-
-  # this assumes that 6 disks are attached at lun 0 through 9
-  echo "Creating partitions and physical volumes"
-  pvcreate -ff -y  /dev/disk/azure/scsi1/lun6
-  pvcreate -ff -y  /dev/disk/azure/scsi1/lun7
-  pvcreate  -ff -y /dev/disk/azure/scsi1/lun8
-
-  echo "logicalvols start" >> /tmp/parameter.txt
-  #shared volume creation
-  sharedvglun="/dev/disk/azure/scsi1/lun0"
-  vgcreate sharedvg $sharedvglun
-  lvcreate -l 100%FREE -n sharedlv sharedvg 
- 
-  #usr volume creation
-  usrsapvglun="/dev/disk/azure/scsi1/lun1"
-  vgcreate usrsapvg $usrsapvglun
-  lvcreate -l 100%FREE -n usrsaplv usrsapvg
-
-  #backup volume creation
-  backupvg1lun="/dev/disk/azure/scsi1/lun2"
-  backupvg2lun="/dev/disk/azure/scsi1/lun3"
-  vgcreate backupvg $backupvg1lun $backupvg2lun
-  lvcreate -l 100%FREE -n backuplv backupvg 
-
-  #data volume creation
-  datavg1lun="/dev/disk/azure/scsi1/lun4"
-  datavg2lun="/dev/disk/azure/scsi1/lun5"
-  datavg3lun="/dev/disk/azure/scsi1/lun6"
-  vgcreate datavg $datavg1lun $datavg2lun $datavg3lun 
-  PHYSVOLUMES=3
-  STRIPESIZE=64
-  lvcreate -i$PHYSVOLUMES -I$STRIPESIZE -l 100%FREE -n datalv datavg
-
-  #log volume creation
-  logvg1lun="/dev/disk/azure/scsi1/lun7"
-  logvg2lun="/dev/disk/azure/scsi1/lun8"
-  vgcreate logvg $logvg1lun $logvg2lun
-  PHYSVOLUMES=2
-  STRIPESIZE=32
-  lvcreate -i$PHYSVOLUMES -I$STRIPESIZE -l 100%FREE -n loglv logvg
-  mount -t xfs /dev/logvg/loglv /hana/log   
-echo "/dev/mapper/logvg-loglv /hana/log xfs defaults 0 0" >> /etc/fstab
-
-  mkfs.xfs /dev/datavg/datalv
-  mkfs.xfs /dev/logvg/loglv
-  mkfs -t xfs /dev/sharedvg/sharedlv 
-  mkfs -t xfs /dev/backupvg/backuplv 
-  mkfs -t xfs /dev/usrsapvg/usrsaplv
-echo "logicalvols end" >> /tmp/parameter.txt
-fi
+#sed -i -e "s/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/g" -e "s/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=163840/g" /etc/waagent.conf
 
 if [ $VMSIZE == "Standard_M128ms" || [ $VMSIZE == "Standard_M208ms_v2" ]; then
 
@@ -276,77 +182,92 @@ echo "/dev/mapper/backupvg-backuplv /hana/backup xfs defaults 0 0" >> /etc/fstab
 echo "/dev/mapper/usrsapvg-usrsaplv /usr/sap xfs defaults 0 0" >> /etc/fstab
 echo "write to fstab end" >> /tmp/parameter.txt
 
-if [ ! -d "/hana/data/sapbits" ]
- then
- mkdir "/hana/data/sapbits"
+if [ ! -d "/hana/data/sapbits" ]; then
+  mkdir -p "/hana/data/sapbits"
 fi
 
-HANAVER=${HANAVER^^}
-if [ "${HANAVER}" = "SAP HANA PLATFORM EDITION 2.0 SPS01 REV 10 (51052030)" ]
-then
-  hanapackage="51052030"
+if [ "$6" == "2.0" ]; then
+  cd /hana/data/sapbits
+  echo "hana 2.0 download start" >> /tmp/parameter.txt
+  /usr/bin/wget --quiet $Uri/SapBits/md5sums
+  /usr/bin/wget --quiet $Uri/SapBits/51053381_part1.exe
+  /usr/bin/wget --quiet $Uri/SapBits/51053381_part2.rar
+  /usr/bin/wget --quiet $Uri/SapBits/51053381_part3.rar
+  /usr/bin/wget --quiet $Uri/SapBits/51053381_part4.rar
+  /usr/bin/wget --quiet "https://raw.githubusercontent.com/wkdang/SAPonAzure/master/hdbinst1.cfg"
+  echo "hana 2.0 download end" >> /tmp/parameter.txt
+
+  date >> /tmp/testdate
+  cd /hana/data/sapbits
+
+  echo "hana 2.0 unrar start" >> /tmp/parameter.txt
+  cd /hana/data/sapbits
+  unrar x 51053381_part1.exe
+  echo "hana 2.0 unrar end" >> /tmp/parameter.txt
+
+  echo "hana 2.0 prepare start" >> /tmp/parameter.txt
+  cd /hana/data/sapbits
+
+  cd /hana/data/sapbits
+  myhost=`hostname`
+  sedcmd="s/REPLACE-WITH-HOSTNAME/$myhost/g"
+  sedcmd2="s/\/hana\/shared\/sapbits\/51052325/\/hana\/data\/sapbits\/51053381/g"
+  sedcmd3="s/root_user=root/root_user=$HANAUSR/g"
+  #sedcmd4="s/root_password=AweS0me@PW/root_password=$HANAPWD/g"
+  sedcmd4="s/password=AweS0me@PW/password=$HANAPWD/g"
+  sedcmd5="s/sid=H10/sid=$HANASID/g"
+  sedcmd6="s/number=00/number=$HANANUMBER/g"
+  #cat hdbinst1.cfg | sed $sedcmd | sed $sedcmd2 | sed $sedcmd3 | sed $sedcmd4 | sed $sedcmd5 | sed $sedcmd6 > hdbinst-local.cfg
+  cp -f /hana/data/sapbits/hdbinst1.cfg /hana/data/sapbits/hdbinst-local.cfg
+  sed -i -e $sedcmd -e $sedcmd2 -e $sedcmd3 -e $sedcmd4 -e $sedcmd5 -e $sedcmd6 /hana/data/sapbits/hdbinst-local.cfg
+  echo "hana 2.0 prepare end" >> /tmp/parameter.txt
+
+  echo "install hana 2.0 start" >> /tmp/parameter.txt
+  cd /hana/data/sapbits/51053381/DATA_UNITS/HDB_LCM_LINUX_X86_64
+  /hana/data/sapbits/51053381/DATA_UNITS/HDB_LCM_LINUX_X86_64/hdblcm -b --configfile /hana/data/sapbits/hdbinst-local.cfg
+  echo "Log file written to '/var/tmp/hdb_H10_hdblcm_install_xxx/hdblcm.log' on host 'saphanaarm'." >> /tmp/parameter.txt
+  echo "install hana 2.0 end" >> /tmp/parameter.txt
+
 else
-  echo "not 51052030"
-  if [ "$HANAVER" = "SAP HANA PLATFORM EDITION 2.0 SPS02 (51052325)" ]
-  then
-    hanapackage="51052325"
-  else
-  echo "not 51052325"
-    if [ "$HANAVER" = "SAP HANA PLATFORM EDITION 2.0 SPS03 REV30 (51053061)" ]
-    then
-      hanapackage="51053061"
-    else
-      echo "not 51053061, default to 51052325"
-      hanapackage="51052325"
-    fi
-  fi
-fi
-
-
-#!/bin/bash
-cd /hana/data/sapbits
-echo "hana download start" >> /tmp/parameter.txt
+  cd /hana/data/sapbits
+echo "hana 1.0 download start" >> /tmp/parameter.txt
 /usr/bin/wget --quiet $Uri/SapBits/md5sums
-/usr/bin/wget --quiet $Uri/SapBits/${hanapackage}_part1.exe
-/usr/bin/wget --quiet $Uri/SapBits/${hanapackage}_part2.rar
-/usr/bin/wget --quiet $Uri/SapBits/${hanapackage}_part3.rar
-/usr/bin/wget --quiet $Uri/SapBits/${hanapackage}_part4.rar
-/usr/bin/wget --quiet "https://raw.githubusercontent.com/AzureCAT-GSI/SAP-HANA-ARM/master/hdbinst.cfg"
-echo "hana download end" >> /tmp/parameter.txt
+/usr/bin/wget --quiet $Uri/SapBits/51052383_part1.exe
+/usr/bin/wget --quiet $Uri/SapBits/51052383_part2.rar
+/usr/bin/wget --quiet $Uri/SapBits/51052383_part3.rar
+/usr/bin/wget --quiet "https://raw.githubusercontent.com/wkdang/SAPonAzure/master/hdbinst.cfg"
+echo "hana 1.0 download end" >> /tmp/parameter.txt
 
 date >> /tmp/testdate
 cd /hana/data/sapbits
 
-echo "hana unrar start" >> /tmp/parameter.txt
-#!/bin/bash
+echo "hana 1.0 unrar start" >> /tmp/parameter.txt
 cd /hana/data/sapbits
-unrar x ${hanapackage}_part1.exe
-echo "hana unrar end" >> /tmp/parameter.txt
+unrar x 51052383_part1.exe
+echo "hana 1.0 unrar end" >> /tmp/parameter.txt
 
-echo "hana prepare start" >> /tmp/parameter.txt
+echo "hana 1.0 prepare start" >> /tmp/parameter.txt
 cd /hana/data/sapbits
 
-#!/bin/bash
 cd /hana/data/sapbits
 myhost=`hostname`
 sedcmd="s/REPLACE-WITH-HOSTNAME/$myhost/g"
-sedcmd2="s/\/hana\/shared\/sapbits\/51052325/\/hana\/data\/sapbits\/${hanapackage}/g"
+sedcmd2="s/\/hana\/shared\/sapbits\/51052325/\/hana\/data\/sapbits\/51052383/g"
 sedcmd3="s/root_user=root/root_user=$HANAUSR/g"
-sedcmd4="s/AweS0me@PW/$HANAPWD/g"
+sedcmd4="s/password=AweS0me@PW/password=$HANAPWD/g"
 sedcmd5="s/sid=H10/sid=$HANASID/g"
 sedcmd6="s/number=00/number=$HANANUMBER/g"
-cat hdbinst.cfg | sed $sedcmd | sed $sedcmd2 | sed $sedcmd3 | sed $sedcmd4 | sed $sedcmd5 | sed $sedcmd6 > hdbinst-local.cfg
-echo "hana preapre end" >> /tmp/parameter.txt
+#cat hdbinst.cfg | sed $sedcmd | sed $sedcmd2 | sed $sedcmd3 | sed $sedcmd4 | sed $sedcmd5 | sed $sedcmd6 > hdbinst-local.cfg
+cp -f /hana/data/sapbits/hdbinst.cfg /hana/data/sapbits/hdbinst-local.cfg
+sed -i -e $sedcmd -e $sedcmd2 -e $sedcmd3 -e $sedcmd4 -e $sedcmd5 -e $sedcmd6 /hana/data/sapbits/hdbinst-local.cfg
+echo "hana 1.0 prepare end" >> /tmp/parameter.txt
 
-#put host entry in hosts file using instance metadata api
-VMIPADDR=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/privateIpAddress?api-version=2017-08-01&format=text"`
-VMNAME=`hostname`
-cat >>/etc/hosts <<EOF
-$VMIPADDR $VMNAME
-EOF
+echo "install hana 1.0 start" >> /tmp/parameter.txt
+cd /hana/data/sapbits/51052383/DATA_UNITS/HDB_LCM_LINUX_X86_64
+/hana/data/sapbits/51052383/DATA_UNITS/HDB_LCM_LINUX_X86_64/hdblcm -b --configfile /hana/data/sapbits/hdbinst-local.cfg
+echo "Log file written to '/var/tmp/hdb_H10_hdblcm_install_xxx/hdblcm.log' on host 'saphanaarm'." >> /tmp/parameter.txt
+echo "install hana 1.0 end" >> /tmp/parameter.txt
 
-#!/bin/bash
-echo "install hana start" >> /tmp/parameter.txt
-cd /hana/data/sapbits/${hanapackage}/DATA_UNITS/HDB_LCM_LINUX_X86_64
-/hana/data/sapbits/${hanapackage}/DATA_UNITS/HDB_LCM_LINUX_X86_64/hdblcm -b --configfile /hana/data/sapbits/hdbinst-local.cfg
-echo "install hana end" >> /tmp/parameter.txt
+
+fi
+shutdown -r 1
